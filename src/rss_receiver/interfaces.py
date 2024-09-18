aeb8c5ebb2b0c7e12b57d6e584a8db5abbf5f7aa
Mike Metcalfe <mike@webtide.co.za>
2024-09-18 14:12:30 +0200

    Fixed error in blowser layer interface

--- src/rss_receiver/interfaces.py
+++ src/rss_receiver/interfaces.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 from zope.interface import Interface
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+
+
+class IRssReceiverLayer(IDefaultBrowserLayer):
+    """Marker interface that defines a browser layer."""
 
 
 class IRSSMixerFeed(Interface):
